e96885a4743c392df12d2502af09a3200a010232
Ma Haijun <mahaijuns@gmail.com>
2013-07-27 23:32:07 +0800

    tiny210: change MACH_TYPE according to latest kernel

--- include/configs/tiny210.h
+++ include/configs/tiny210.h
@@ -89,7 +89,7 @@
 #define CONFIG_CMDLINE_EDITING
 
 /* MACH_TYPE_MINI210 macro will be removed once added to mach-types */
-#define MACH_TYPE_TINY210		2456
+#define MACH_TYPE_TINY210		4448
 #define CONFIG_MACH_TYPE		MACH_TYPE_TINY210
 
 /* Size of malloc() pool */
